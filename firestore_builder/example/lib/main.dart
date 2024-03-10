ed9691d5d3bec76804ac2497c7074222a2392715
Jordan ALCARAZ <jordan.alcaraz.pro@gmail.com>
2024-03-10 09:21:26 +0400

    Add firebase dep and ignore generated files

--- firestore_builder/example/lib/main.dart
+++ firestore_builder/example/lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:example/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
