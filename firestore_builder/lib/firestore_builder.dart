1b2c031fab8d865f6ab17fa5374b893a6ad153c0
Jordan ALCARAZ <jordan.alcaraz.pro@gmail.com>
2024-04-14 07:55:55 +0400

    Export all models

--- firestore_builder/lib/firestore_builder.dart
+++ firestore_builder/lib/firestore_builder.dart
@@ -1,7 +1,4 @@
 /// Firestore Builder
 library firestore_builder;
 
-export 'src/models/collection.dart';
-export 'src/models/collection_field.dart';
-export 'src/models/field_type.dart';
-export 'src/models/yaml_config.dart';
+export 'src/models/models.dart';
---
+++ firestore_builder/lib/src/models/models.dart
@@ -0,0 +1,4 @@
+export 'collection.dart';
+export 'collection_field.dart';
+export 'field_type.dart';
+export 'yaml_config.dart';
